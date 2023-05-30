93d4d83ac81e198dc3cff536aa4d5c05d208fc4c
Hemel Sharker Akash <44114317+HSAkash@users.noreply.github.com>
2023-05-30 08:42:34 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
+sudo apt install build-essential
+
 pip3 install -r requirements.txt
 python3 setup.py  develop
 pip3 install cython
 pip3 install 'git+https://github.com/cocodataset/cocoapi.git#subdirectory=PythonAPI'
 pip install cython_bbox
 pip install onemetric
+
+pip install -q loguru lap thop
